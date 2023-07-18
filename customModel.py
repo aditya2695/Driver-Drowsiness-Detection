a91050005aadfc070252bdff051e9b5a78ededc1
Aditya Anandarajan <adityaarajan@gmail.com>
2023-07-18 22:18:55 +0100

    Create customModel.py

---
+++ customModel.py
@@ -0,0 +1,32 @@
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+
+# Define the CNN architecture
+class CustomCNN(nn.Module):
+    def __init__(self, num_classes=4):
+        super(CustomCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(32 * 56 * 56, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
